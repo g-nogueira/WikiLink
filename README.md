5b26f30887bf95eff4fd1af4548f83b5c999a13f
Gustavo Henrique dos Reis Nogueira <gustavornogueir@gmail.com>
2018-02-13 23:12:05 +0000

    ✏Using bundled WikiRepo.js now

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# WikiLink-Notes
-Wikilink extension with the possibility of notes taking
+# WikiLink
+A handy chrome extension to fast search terms on Wikipedia and write quick notes.
+It will be compatible with Evernote and perhaps OneNote.
--- repository/WikiRepo.js
+++ repository/WikiRepo.js
@@ -1,6 +1,8 @@
 (async function () {
     'using strict';
 
+    const franc = require('franc');
+
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.module === 'wikiRepo') {
             wikiRepo[message.method](message.key, message.range)
@@ -19,22 +21,22 @@
          * @returns {Promise.<object>} Returns a Promise that resolves to an object with title and body properties.
          */
         searchTerm(term, range) {
-            const article = {title: '', body: ''};
+            const article = { title: '', body: '' };
 
             return new Promise(async (resolve, reject) => {
 
-                let language = await http.get(`http://localhost:8081/?m=language&v=${range}`);
-                language = JSON.parse(language).language === 'und'? 'en': JSON.parse(language).language;
+                let language = this.identifyLanguage(range);
+                language = language.language === 'und' ? 'en' : language.language;
 
                 const searchResponse = await http.get(`https://${language}.wikipedia.org/w/api.php?action=opensearch&search=${term}&limit=2&namespace=0&format=json`);
                 const resultsArray = JSON.parse(searchResponse);
-                
+
                 const titles = resultsArray[1];
                 const articles = resultsArray[2];
 
                 try {
                     article.title = titles[0];
-                    article.body = articles[0].includes('refer to:')||articles[0].includes('referir-se a:') ? articles[1] : articles[0];
+                    article.body = articles[0].includes('refer to:') || articles[0].includes('referir-se a:') ? articles[1] : articles[0];
                 } catch (error) {
                     console.warn(`Couldn't get an article for the term "${term}".`);
                 }
@@ -72,6 +74,20 @@
                     resolve(imageInfo);
                 }
             });
+        }
+
+        identifyLanguage(extract) {
+            const regexUTF8 = /([^\u0000-\u0040\u005B-\u0060\u007B-\u00BF\u02B0-\u036F\u00D7\u00F7\u2000-\u2BFF])+/g;
+            const text = extract.match(regexUTF8).toString();
+            const whitelist = ['por', 'eng', 'spa', 'deu', 'fra', 'ita', 'rus'];
+            const francRes = { language: franc(extract, { whitelist: whitelist }) };
+            const languages = {
+                por: 'pt', eng: 'en', spa: 'es', deu: 'de',
+                fra: 'fr', ita: 'it', rus: 'ru'
+            };
+
+
+            return { language: languages[francRes.language] };
         }
     }
 
