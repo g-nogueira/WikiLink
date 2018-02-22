a5aa0bb5739156426f0c5fc55cb20b8d33cc54e9
PTE ESPA <pte.espa@espadm.local>
2018-02-22 13:09:28 +0000

    Options Page

---
+++ optionsPage/index.js
@@ -0,0 +1,16 @@
+(function (){
+    'use strict';
+    const el = elem => document.querySelector(elem);
+    const els = elem => document.querySelectorAll(elem);
+
+    name();
+
+    function name() {
+        const shortcuts = els('.self-js-showKey');
+        shortcuts.forEach(el => {
+            el.addEventListener('keypress', ev => {
+                ev.currentTarget.textContent += `+ ${String.fromCharCode(ev.charCode)}`;
+            });
+        });
+    }
+})();
